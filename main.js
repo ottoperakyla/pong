9cae003e5688d747ac9d8fed385bfc92a480e414
Otto Peräkylä <otto.perakyla@veikkaus.fi>
2018-12-14 20:24:16 +0200

    remove deps

--- main.js
+++ main.js
@@ -42,19 +42,19 @@ function drawBoard() {
   ctx.fillStyle = 'rgb(173, 173, 173)'
   ctx.fillRect(0, 0, canvas.width, borderSize)
   ctx.fillRect(0, canvas.height - borderSize, canvas.width, borderSize)
-  
+
   const dashCount = Math.ceil(canvas.height / borderSize)
-  
+
   for (let i = 0; i < dashCount; i++) {
     ctx.fillRect(canvas.width / 2 - borderSize / 2, i*borderSize*2+borderSize, borderSize, borderSize)
-  }  
+  }
 }
 
 function drawScore(a, b) {
   const fontSize = 60
   ctx.font = `normal bold ${fontSize}px Helvetica`
   ctx.fillText(''+a, canvas.width / 2 - fontSize, borderSize + fontSize)
-  ctx.fillText(''+b, canvas.width / 2 + fontSize / 2, borderSize + fontSize)  
+  ctx.fillText(''+b, canvas.width / 2 + fontSize / 2, borderSize + fontSize)
 }
 
 function drawPaddle(x, y) {
@@ -81,12 +81,12 @@ function drawBall(ball) {
   ctx.fillRect(ball.x, ball.y, ballSize, ballSize)
   ball.x += ball.vx
   ball.y += ball.vy
-  
+
   const collisionBottom = ball.y > canvas.height - borderSize - ballSize
   const collisionTop = ball.y < borderSize
   const collisionLeft = ball.x < players[0].x + paddle.width && ball.y > players[0].y && ball.y < players[0].y + paddle.height//todo: copypasta
   const collisionRight = ball.x > players[1].x - paddle.width && ball.y > players[1].y && ball.y < players[1].y + paddle.height
-  
+
   if (collisionBottom || collisionTop) {
     ball.vy = -ball.vy
   } else if (collisionLeft || collisionRight) {
@@ -98,15 +98,15 @@ function drawBall(ball) {
   }
 }
 
-document.addEventListener('keydown', event => {
+document.addEventListener('keypress', event => {
   switch (event.key) {
-    case 'w': 
+    case 'w':
       players[0].y = Math.max(borderSize, players[0].y - paddle.vy)
       break
     case 's':
       players[0].y = Math.min(canvas.height - paddle.height - borderSize, players[0].y + paddle.vy)
       break
-    case 'ArrowUp': 
+    case 'ArrowUp':
       players[1].y = Math.max(borderSize, players[1].y - paddle.vy)
       break
     case 'ArrowDown':
